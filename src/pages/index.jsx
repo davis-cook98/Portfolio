ace2caabba2df3f3ab60db0d3a534b76f9e304dd
Davis <daviscook@outlook.com>
2019-08-18 14:58:04 -0400

    Fix formatting

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,38 +7,45 @@ import classNames from 'classnames';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
+import { Grid } from '@material-ui/core';
 
 const Index = () => (
   <Layout>
-    <Container>
-      <Typography
-        component="h1"
-        variant="h2"
-        align="absolute"
-        color="textPrimary"
-        gutterBottom
-      >
-        davis cook
-      </Typography>
-      <Typography variant="h6" align="center" color="textSecondary" paragraph>
-        a student at seton hall interested in data analysis
-      </Typography>
-      <Typography variant="h6" align="center" color="textSecondary" paragraph>
-        <Link is={GatsbyLink} to="/about/">
-          about
-        </Link>
-        -
-        <Link is={GatsbyLink} to="/projects/">
-          projects
-        </Link>
-        -
-        <Link is={GatsbyLink} to="/quotes/">
-          quotes
-        </Link>
-      </Typography>
-    </Container>
+    <Grid
+      container
+      alignItems="center"
+      justify="center"
+      style={{ minHeight: '90vh' }}
+    >
+      <Container>
+        <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          color="textPrimary"
+          gutterBottom
+        >
+          davis cook
+        </Typography>
+        <Typography variant="h6" align="center" color="textSecondary" paragraph>
+          a student at seton hall interested in data analysis
+        </Typography>
+        <Typography variant="h6" align="center" color="textSecondary" paragraph>
+          <Link is={GatsbyLink} to="/about/">
+            about
+          </Link>
+          -
+          <Link is={GatsbyLink} to="/projects/">
+            projects
+          </Link>
+          -
+          <Link is={GatsbyLink} to="/quotes/">
+            quotes
+          </Link>
+        </Typography>
+      </Container>
+    </Grid>
   </Layout>
 );
-
 
 export default Index;
